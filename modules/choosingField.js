23662622caf571a999a28c3ec88613623aa0c7c3
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-10-21 00:29:37 +0200

    Creating input from user for field

---
+++ modules/choosingField.js
@@ -0,0 +1,17 @@
+modules.exports {
+    chooseWidth : (userInputWidth) => {
+    let inputtedWidth = userInputWidth.toString().trim();
+    (inputtedWidth, newPlay)
+    newPlay.print()
+  }
+    chooseHeight : (userInputWidth) => {
+    let inputtedWidth = inputtedWidth.toString().trim();
+    move(input, newPlay)
+    newPlay.print()
+  }
+    chooseHoles : (userInputWidth) => {
+    let inputtedWidth = inputtedWidth.toString().trim();
+    move(input, newPlay)
+    newPlay.print()
+  }
+}
