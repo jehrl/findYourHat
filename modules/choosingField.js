866ce95fb4ed68efa549442c162ae91eb7c70dff
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-10-21 00:31:49 +0200

    correcting the syntax mistakes

--- modules/choosingField.js
+++ modules/choosingField.js
@@ -1,14 +1,14 @@
-modules.exports {
+module.exports = {
     chooseWidth : (userInputWidth) => {
     let inputtedWidth = userInputWidth.toString().trim();
     (inputtedWidth, newPlay)
     newPlay.print()
-  }
+  },
     chooseHeight : (userInputWidth) => {
     let inputtedWidth = inputtedWidth.toString().trim();
     move(input, newPlay)
     newPlay.print()
-  }
+  },
     chooseHoles : (userInputWidth) => {
     let inputtedWidth = inputtedWidth.toString().trim();
     move(input, newPlay)
